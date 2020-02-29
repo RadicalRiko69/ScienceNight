1baa1b5275df829df43ad916b79e6e0b66024fb8
Giovanni Shawn <rhythmviolatorz@gmail.com>
2020-02-28 19:22:14 -0600

    BGA update

--- BGAnimations/ScreenWithMenuElements background/default.lua
+++ BGAnimations/ScreenWithMenuElements background/default.lua
@@ -2,7 +2,7 @@ local t = Def.ActorFrame {};
 
 t[#t+1] = Def.ActorFrame {
 	LoadActor("BG") .. {
-		InitCommand=cmd(zoom,1;x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y);
+		InitCommand=cmd(zoom,0.5;x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y);
 	};
 };
 
@@ -12,7 +12,7 @@ t[#t+1] = Def.ActorFrame {
 	Def.ActorFrame {
 		InitCommand=cmd(hide_if,hideFancyElements;);
 		LoadActor("_checkerboard") .. {
-			InitCommand=cmd(rotationy,0;rotationz,0;rotationx,-90/4*3.5;zoomto,SCREEN_WIDTH*18,SCREEN_HEIGHT*18;customtexturerect,0,0,SCREEN_WIDTH*24/256,SCREEN_HEIGHT*24/256);
+			InitCommand=cmd(rotationy,0;rotationz,0;rotationx,-90/4*3.5;zoomto,SCREEN_WIDTH*9,SCREEN_HEIGHT*9;addy,50;customtexturerect,0,0,SCREEN_WIDTH*16/256,SCREEN_HEIGHT*16/256);
 			OnCommand=cmd(texcoordvelocity,0,0.1;diffuse,color("#ff7dad");fadetop,5);
 		};
 	};
@@ -30,7 +30,7 @@ t[#t+1] = Def.ActorFrame {
 
 t[#t+1] = Def.ActorFrame {
 	LoadActor("moon") .. {
-		InitCommand=cmd(zoom,0.25;x,SCREEN_CENTER_X+500;y,SCREEN_CENTER_Y-400;diffusealpha,0.5);
+		InitCommand=cmd(zoom,0.125;x,SCREEN_CENTER_X+250;y,SCREEN_CENTER_Y-200;diffusealpha,0.5);
 	};
 };
 
