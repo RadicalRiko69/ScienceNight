d41813fb23e1706f216100059e6aac8d243cc046
Giovanni Shawn <rhythmviolatorz@gmail.com>
2020-03-02 14:33:00 -0600

    title screen changes

--- BGAnimations/ScreenTitleMenu decorations/default.lua
+++ BGAnimations/ScreenTitleMenu decorations/default.lua
@@ -3,11 +3,11 @@ return Def.ActorFrame {
 		QUESTMODE:Reset();
 	end;
     LoadActor("science_night running")..{
-        InitCommand=cmd(zoom,0.8;x,SCREEN_LEFT-500;y,SCREEN_CENTER_Y);
-        OnCommand=cmd(sleep,2;decelerate,1.5;x,SCREEN_CENTER_X-520);
+        InitCommand=cmd(zoom,0.4;x,SCREEN_LEFT-500;y,SCREEN_CENTER_Y);
+        OnCommand=cmd(sleep,2;decelerate,1.5;x,SCREEN_CENTER_X-220);
     };
     LoadActor("logo")..{
-        InitCommand=cmd(zoom,1);
-        OnCommand=cmd(sleep,2;decelerate,0.5;addy,-120;zoom,0.4);
+        InitCommand=cmd(x,235;y,135;zoom,0.5);
+        OnCommand=cmd(sleep,2;decelerate,0.5;addy,-150;addx,-140;zoom,0.25);
     };
 };
--- Graphics/ScreenTitleMenu scroll.lua
+++ Graphics/ScreenTitleMenu scroll.lua
@@ -1,9 +1,11 @@
 local gc = Var("GameCommand");
+local index = gc:GetIndex()+1;
 
 return Def.ActorFrame {
 	LoadFont("_charter bt 40px") .. {
 		Text=THEME:GetString("ScreenTitleMenu",gc:GetText());
-		GainFocusCommand=cmd(stoptweening;decelerate,.1;zoom,1;diffuse,color("#FFFFFF"));
-		LoseFocusCommand=cmd(stoptweening;decelerate,.1;zoom,0.9;diffuse,color("#4d4d4d"));
+		OnCommand=cmd(horizalign,right;addx,-100;diffusealpha,0;sleep,3+index*.1;decelerate,.2;addx,100;diffusealpha,1);
+		GainFocusCommand=cmd(stoptweening;linear,.1;zoom,0.8);
+		LoseFocusCommand=cmd(stoptweening;linear,.1;zoom,0.7);
 	};
 };
