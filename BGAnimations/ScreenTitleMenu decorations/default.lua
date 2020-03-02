b2110e026ef3779ddcca1d2fe853ec3f0326f6bc
Giovanni Shawn <rhythmviolatorz@gmail.com>
2020-03-02 14:48:53 -0600

    title menu info

--- BGAnimations/ScreenTitleMenu decorations/default.lua
+++ BGAnimations/ScreenTitleMenu decorations/default.lua
@@ -10,4 +10,11 @@ return Def.ActorFrame {
         InitCommand=cmd(x,235;y,135;zoom,0.5);
         OnCommand=cmd(sleep,2;decelerate,0.5;addy,-150;addx,-140;zoom,0.25);
     };
+    LoadFont("_alternategotno2 40px")..{
+		InitCommand=cmd(horizalign,left;x,SCREEN_LEFT+20;y,SCREEN_BOTTOM-40);
+		OnCommand=function(self)
+			self:settext("Project by Team Sushi\nProgramming by RhythmLunatic\nIllustration by Giovanni Shawn");
+			(cmd(finishtweening;zoom,0.4)) (self)
+		end;
+	};
 };
