9d941c86a32717be968937f35612553ede08ee99
Giovanni Shawn <rhythmviolatorz@gmail.com>
2020-03-03 21:21:19 -0600

    transitions and stuff
    oh and new music and sfx

--- BGAnimations/ScreenTitleMenu decorations/default.lua
+++ BGAnimations/ScreenTitleMenu decorations/default.lua
@@ -7,10 +7,10 @@ return Def.ActorFrame {
         InitCommand=cmd(zoom,0.4;x,SCREEN_LEFT-500;y,SCREEN_CENTER_Y);
         OnCommand=cmd(sleep,2;decelerate,1.5;x,SCREEN_CENTER_X-220);
     };
-    LoadActor("logo")..{
-        InitCommand=cmd(x,235;y,135;zoom,0.5);
-        OnCommand=cmd(sleep,2;decelerate,0.5;addy,-150;addx,-140;zoom,0.25);
-    };
+    LoadActor(THEME:GetPathG("", "logo")) .. {
+		InitCommand=cmd(zoomto,SCREEN_WIDTH,SCREEN_HEIGHT;Center),
+		OnCommand=cmd(diffusealpha,1;sleep,2;decelerate,0.5;zoom,0.25;addy,-220;addx,-260)
+	};
     LoadFont("_alternategotno2 40px")..{
 		InitCommand=cmd(horizalign,left;x,SCREEN_LEFT+20;y,SCREEN_BOTTOM-40);
 		OnCommand=function(self)
